dd7a7a956cb36b595685b560ea8d4ac4c4bcd9a7
bingo8670 <307286130@qq.com>
2018-12-26 23:56:27 +0800

    12.26

---
+++ favorite_languages.py
@@ -0,0 +1,14 @@
+favoriate_languages = {
+        'jen': ['python', 'ruby'],
+        'sarah': ['c'],
+        'edward': ['ruby', 'go'],
+        'phil': ['python', 'haskell'],
+        }
+
+for name, languages in favoriate_languages.items():
+    if len(languages) == 1:
+        print("\n" + name.title() + "'s favorite language is:")
+    else:
+        print("\n" + name.title() + "'s favorite languages are:")
+    for language in languages:
+        print("\t" + language.title())
